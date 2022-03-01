55b0e98a9099d447bf3b555b5f1b9524c796f137
ening <ening6649@yahoo.com>
2022-03-01 17:59:28 -0500

    added footer and style

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './styles.css'
 
 function Footer() {
     return (
@@ -7,24 +8,52 @@ function Footer() {
                 Ning's Portfolio
                
             </h1>
-            <nav>
-                <ul>
-                    <li>
-                        <a href="#about-me">About Me</a>
-                    </li>
-                    <li>
-                        <a href="#what-i-do">What I Do</a>
-                    </li>
-                    <li>
-                      <a href="#my-applications">My Applications</a>
-                    </li>
-                    <li>
-                      <a href="#contact-me">Contact Me</a>
-                    </li>
-                </ul>
-            </nav>
+            <div>
+            <div class="social"><a href="#"><i class="icon ion-social-instagram"></i></a><a href="#"><i class="icon ion-social-snapchat"></i></a><a href="#"><i class="icon ion-social-twitter"></i></a><a href="#"><i class="icon ion-social-facebook"></i></a></div>
+            <ul class="list-inline">
+                <li class="list-inline-item"><a href="#">Home</a></li>
+                <li class="list-inline-item"><a href="#">Services</a></li>
+                <li class="list-inline-item"><a href="#">About</a></li>
+                <li class="list-inline-item"><a href="#">Terms</a></li>
+                <li class="list-inline-item"><a href="#">Privacy Policy</a></li>
+            </ul>
+            <p class="copyright">Company Name © 2018</p>
+            </div>
         </section>
     )
 }
 
 export default Footer;
+
+// <!DOCTYPE html>
+// <html>
+
+// <head>
+//     <meta charset="utf-8">
+//     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//     <title>Untitled</title>
+//     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css">
+//     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
+//     <link rel="stylesheet" href="assets/css/style.css">
+// </head>
+
+// <body>
+//     <div class="footer-basic">
+//         <footer>
+//             <div class="social"><a href="#"><i class="icon ion-social-instagram"></i></a><a href="#"><i class="icon ion-social-snapchat"></i></a><a href="#"><i class="icon ion-social-twitter"></i></a><a href="#"><i class="icon ion-social-facebook"></i></a></div>
+//             <ul class="list-inline">
+//                 <li class="list-inline-item"><a href="#">Home</a></li>
+//                 <li class="list-inline-item"><a href="#">Services</a></li>
+//                 <li class="list-inline-item"><a href="#">About</a></li>
+//                 <li class="list-inline-item"><a href="#">Terms</a></li>
+//                 <li class="list-inline-item"><a href="#">Privacy Policy</a></li>
+//             </ul>
+//             <p class="copyright">Company Name © 2018</p>
+//         </footer>
+//     </div>
+//     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+//     <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/js/bootstrap.bundle.min.js"></script>
+// </body>
+
+// </html>
+
